d8eba24e4cef56b67b1b2e0b947e25702581b631
newBirdX <92900593+newBirdX@users.noreply.github.com>
2022-02-18 14:29:58 +0800

    富文本完成

--- src/pages/rich/index.jsx
+++ src/pages/rich/index.jsx
@@ -1,12 +1,53 @@
 import React, { Component } from 'react'
-import { Card } from 'antd'
+import { Card ,Button,Modal} from 'antd'
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
+import draftjs from 'draftjs-to-html';
 export default class Rich extends Component {
+     state={
+         showRich:false  //显示文本内容框
+     }
+     onEditorStateChange=(editorState)=>{
+         this.setState({
+             editorState
+         })
+     }
+     handdleClearContent=()=>{
+        this.setState({
+            editorState:""
+        })
+     }
+     handdleGetText=()=>{
+        this.setState({
+            showRich:true
+        })
+     }
+     onEditorChange=(contentState)=>{
+        this.setState({
+            contentState
+        })
+     }
     render() {
+        let {editorState,contentState,showRich}=this.state;
         return (
             <div>
-                <Card title="富文本编辑器">
-                    
+                <Card>
+                    <Button type="primary" style={{marginRight:"10px"}} onClick={this.handdleClearContent}>清空内容</Button>
+                    <Button type="primary" onClick={this.handdleGetText}>获取文本内容</Button>
                 </Card>
+                <Card title="富文本编辑器">
+                    <Editor
+                        editorState={editorState}
+                        toolbarClassName="toolbarClassName"
+                        wrapperClassName="wrapperClassName"
+                        editorClassName="editorClassName"
+                        onEditorStateChange={this.onEditorStateChange}
+                        onContentStateChange={this.onEditorChange}
+                    />;
+                </Card>
+                <Modal title="文本内容" visible={showRich} onCancel={()=>{this.setState({showRich:false})}} footer={null}>
+                    {draftjs(contentState)}
+                </Modal>
             </div>
         )
     }
